acdf4954c792c3b75cc5e13343fe11b737bc6eb1
Marcin Owerczuk <muzieh@gmail.com>
2019-05-20 23:13:55 +0100

    object instantiation and some tests

--- src/Bign.js
+++ src/Bign.js
@@ -1,15 +1,56 @@
-
-module.exports = {
-    zero: {
-        toString: () => '0'
-    },
-    create: function(val) {
-        if(!val)
-            return '0';
-        
-        return val.toString();
-    },
-    equals: function(other) {
-        return this.value === other.value;
+function parseToIntArray(stringValue) {
+    if(stringValue.indexOf('-') === 0)
+    {
+        this._isNegative = true;
+        stringValue = stringValue.substring(1);
     }
+    
+    this._value = stringValue
+        .split('')
+        .reverse()
+        .map(ch => parseInt(ch))
+        .reduce((prev, curr) => prev.concat(curr), []);
+}
+
+function BigN(val) {
+    this._value = [];
+    this._isNegative = false;
+
+    if (typeof val === 'undefined' || val === null) {
+        return BigN.prototype._zero;
+    }
+    
+    parseToIntArray.call(this, val.toString()); 
+    this.value = Number(val);
+}
+
+BigN.prototype._zero = new BigN(0);
+
+BigN.zero = function() {
+    return BigN.prototype._zero;
+}; 
+
+BigN.prototype.equals = function(other) {
+	throw new Error("not implemented") 
 };
+
+BigN.prototype.toString = function() {
+    const stringValue = this._value.reverse().join(''); 
+    if(this._isNegative) {
+        return `-${stringValue}`;
+    }
+    return stringValue;
+};
+
+// BigN.prototype._convertToArray = function(strValue) {
+//    this._value = strValue 
+//         .split('')
+//         .reverse()
+//         .map(ch => parseInt(ch))
+//         .reduce((prev, curr) => prev.concat(curr), []); 
+// };
+
+
+
+module.exports = BigN;
+
--- test/Bign.js
+++ test/Bign.js
@@ -1,41 +1,52 @@
-const bigN = require('../src/BigN');
+const BigN = require('../src/BigN');
 const should = require('chai').should();
 
 describe('BigN', function () {
 	describe('import library', function () {
 		it('should return object', function () {
-			(bigN).should.not.be.empty;	
+			(typeof BigN).should.equals('function');	
 		})	
 	});
 	
 	describe('initialize object', function () {
 		describe('zero', function () {
 			it('zero returns 0 string', function () {
-				(bigN.zero).toString().should.equals('0');
+				const zero = BigN.zero();
+				zero.toString().should.equals('0');
 			});
 			it('crate object with 0 int value', function() {
-				(bigN.create(0)).toString().should.equals('0')
+				(new BigN(0)).toString().should.equals('0')
 			});
 			it('crate object without parameter', function() {
-				(bigN.create()).toString().should.equals('0')
+				(new BigN()).toString().should.equals('0')
 			});
 			
 		});
 		
 		describe('non zero', function() {
 			it('crate object with int 54 value', function() {
-				(bigN.create(54)).toString().should.equals('54')
+				(new BigN(54)).toString().should.equals('54')
 			});
 			it('crate object with negative parameter', function() {
-				(bigN.create(-983)).toString().should.equals('-983')
+				(new BigN(-983)).toString().should.equals('-983')
+			});
+			
+			it('should handle initialisation from string', function() {
+				const strNumber = '12345678';
+				(new BigN(strNumber)).toString().should.equals(strNumber);
+			});
+			
+			it('should handle initialisation from big string', function() {
+				const strNumber = '123456789012345678901234567890';
+				(new BigN(strNumber)).toString().should.equals(strNumber);
 			});
 			
 		})
 	});
 	
 	describe('instance comparition', function() {
-		it.skip('same instance should be equal', function() {
-			const num1 = bigN.create(10);
+		it('same instance should be equal', function() {
+			const num1 = new BigN(10);
 			(num1.equals(num1)).should.be.true;
 		});
 		it('same value different instance should be equal', function() {
